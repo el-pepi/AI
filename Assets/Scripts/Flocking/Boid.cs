e5ab9eea2c0fa77d7205e79e39009dee4e786903
el-pepi <el-pepi@live.com.ar>
2017-10-12 21:35:45 -0300

    started flocking

---
+++ Assets/Scripts/Flocking/Boid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+	Vector3 target;
+
+	public Vector3 targetDir;
+	public Vector3 dir;
+
+	Transform tSprite;
+
+	void Start () {
+		tSprite = transform.GetChild (0);
+	}
+
+	void Update () {
+		targetDir = target - transform.position;
+		dir = Vector3.Slerp (dir, targetDir, 3f * Time.deltaTime).normalized;
+		transform.position += dir * 5 * Time.deltaTime;
+
+		float rot_z = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		tSprite.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+	}
+
+	public void SetDirTarget(Vector3 v){
+		v.z = 0;
+		target = v;
+	}
+}
---
+++ Assets/Scripts/Flocking/Flocking.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flocking : MonoBehaviour {
+
+	List<Boid> boids = new List<Boid> ();
+
+	public int boidsToSpawn = 1;
+	public GameObject boidPrefab;
+
+	// Use this for initialization
+	void Start () {
+		while (boidsToSpawn > 0) {
+			boidsToSpawn--;
+			boids.Add(Instantiate (boidPrefab).GetComponent<Boid>());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			foreach (Boid b in boids) {
+				b.SetDirTarget(Camera.main.ScreenToWorldPoint (Input.mousePosition));
+			}
+		}
+	}
+}
