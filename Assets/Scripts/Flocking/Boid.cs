7dfccbbbd150fb0f432f807f86684c3b39eef791
el-pepi <el-pepi@live.com.ar>
2017-10-19 21:28:08 -0300

    termine flocking behavior

--- Assets/Scripts/Flocking/Boid.cs
+++ Assets/Scripts/Flocking/Boid.cs
@@ -11,21 +11,67 @@ public class Boid : MonoBehaviour {
 
 	Transform tSprite;
 
+	List<Boid> vecinos = new List<Boid>();
+
+	float speed = 5;
+	float rotSpeed = 3f;
+
 	void Start () {
 		tSprite = transform.GetChild (0);
+
+		speed = Random.Range (3f,7f);
+		rotSpeed = Random.Range (1.5f,4.5f);
 	}
 
 	void Update () {
-		targetDir = target - transform.position;
-		dir = Vector3.Slerp (dir, targetDir, 3f * Time.deltaTime).normalized;
-		transform.position += dir * 5 * Time.deltaTime;
+		//targetDir = target - transform.position;
 
-		float rot_z = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-		tSprite.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+		dir = Vector3.Slerp (dir, targetDir, rotSpeed * Time.deltaTime);
+		dir.Normalize ();
+
+
+		transform.position += dir * speed * Time.deltaTime;
+
+		tSprite.LookAt(transform.position+dir);
 	}
 
 	public void SetDirTarget(Vector3 v){
-		v.z = 0;
-		target = v;
+		//v.z = 0;
+		//target = v;
+		targetDir = v;
+	}
+
+	public void AddVecino(Boid b){
+		vecinos.Add (b);
+	}
+
+	public void ResetVecinos(){
+		vecinos.Clear ();
+	}
+
+	public Vector3 GetFlockCentre(){
+		Vector3 v = transform.position;
+
+		foreach (Boid b in vecinos) {
+			v += b.transform.position;
+		}
+
+		v /= vecinos.Count + 1;
+		return v;
+	}
+
+	public Vector3 Getdir(){
+		return dir;
+	}
+
+	public Vector3 GetFlockAlign(){
+		Vector3 v = dir;
+
+		foreach (Boid b in vecinos) {
+			v += b.Getdir();
+		}
+
+		v /= vecinos.Count + 1;
+		return v;
 	}
 }
--- Assets/Scripts/Flocking/Flocking.cs
+++ Assets/Scripts/Flocking/Flocking.cs
@@ -8,21 +8,55 @@ public class Flocking : MonoBehaviour {
 
 	public int boidsToSpawn = 1;
 	public GameObject boidPrefab;
+	public float flockRange;
 
-	// Use this for initialization
+	Vector3 target;
+	Transform targetT;
+
+	public float separationPeso = 1f;
+	public float cohesionPeso = 1f;
+	public float targetPeso = 1f;
+
 	void Start () {
+		targetT = transform.GetChild (0);
 		while (boidsToSpawn > 0) {
 			boidsToSpawn--;
-			boids.Add(Instantiate (boidPrefab).GetComponent<Boid>());
+			boids.Add(Instantiate (boidPrefab,Random.insideUnitSphere * 10,Quaternion.identity ).GetComponent<Boid>());
 		}
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
-			foreach (Boid b in boids) {
-				b.SetDirTarget(Camera.main.ScreenToWorldPoint (Input.mousePosition));
+			target = (Camera.main.ScreenToWorldPoint (Input.mousePosition + Vector3.forward * Random.Range(2f,20f)));
+			targetT.position = target;
+		}
+
+		for (int i = 0; i < boids.Count; i++) {
+			for (int j = i+1; j < boids.Count; j++) {
+				if (Vector3.Distance (boids [i].transform.position, boids [j].transform.position) <= flockRange) {
+					boids [i].AddVecino (boids [j]);
+					boids [j].AddVecino (boids [i]);
+				}
 			}
+		}
+			
+		foreach (Boid b in boids) {
+			Vector3 center = b.GetFlockCentre ();
+			Vector3 separation = (b.transform.position - center);
+			Vector3 cohesion = separation * -1f;
+			float cohesionW = Mathf.Clamp01(separation.magnitude / flockRange);
+			float separationW = 1f - cohesionW;
+			Vector3 align = b.GetFlockAlign ();
+
+			Vector3 targetdir = (target - b.transform.position).normalized * targetPeso;
+
+			Debug.DrawLine (b.transform.position,b.transform.position + separation.normalized * separationW,Color.red);
+			Debug.DrawLine (b.transform.position,b.transform.position + cohesion.normalized * cohesionW,Color.green);
+			Debug.DrawLine (b.transform.position,b.transform.position + align,Color.blue);
+
+			b.SetDirTarget (((((separation.normalized * separationW) * separationPeso + (cohesion.normalized * cohesionW) * cohesionPeso + align).normalized) + targetdir ).normalized );
+
+			b.ResetVecinos ();
 		}
 	}
 }
